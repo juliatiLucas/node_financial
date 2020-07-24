d613251d886555a103d832cf7e04d7874d161778
juliatileo <juliatileo09@gmail.com>
2020-07-24 16:47:38 -0300

    PORT

--- src/server.ts
+++ src/server.ts
@@ -7,4 +7,4 @@ const app = express()
 app.use(express.json())
 app.use(routes)
 
-app.listen(3000 || process.env.PORT, () => console.log('RUNNING'))
+app.listen(process.env.PORT || 5000, () => console.log('RUNNING'))
