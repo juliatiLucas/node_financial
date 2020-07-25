b1b08d5debdf8b4be651a4d3f855f805ac7caf18
juliatileo <juliatileo09@gmail.com>
2020-07-25 15:38:34 -0300

    bodyparser

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import express from 'express'
 import routes from './routes'
+import bodyParser from 'body-parser'
 import { createConnection } from 'typeorm'
 
 createConnection()
 const app = express()
-app.use(express.json())
+app.use(bodyParser.json())
 app.use(routes)
 
 app.listen(process.env.PORT || 5000, () => console.log('RUNNING'))
